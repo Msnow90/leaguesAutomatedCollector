eb9e8d88346631c60fd92a3887c2b2cc9371fa14
Matthew Snow <mattsnowis@yahoo.com>
2017-01-30 17:35:07 -0500

    Implemented proper db functionality

--- sumid-collector.js
+++ sumid-collector.js
@@ -1,19 +1,23 @@
 var EventEmitter = require("events");
 var fs = require("fs");
 var request = require("request");
+var mongoose = require("mongoose");
 
 var IdCollection = require("../src/models/idcollection");
+
 var apiKey = require("../src/config")["apiKey"];
+var mongoURL = require("../src/config")["mongoURL"];
 
 // config in this dir contains the last parsed summoner id
 // this will prevent a lot of redundancy
-var currentSumId = require("./currentSumId");
+var currentSumId = Number(fs.readFileSync("./currentSumId").toString());
 
 
 
 
 var queueTypes = ["RANKED_SOLO_5x5"];
 
+mongoose.connect(mongoURL);
 
 
 //========== Begin Code of Controller Event Emitter =========//
@@ -25,25 +29,54 @@ var queueTypes = ["RANKED_SOLO_5x5"];
 // automated interval
 var requestController = new EventEmitter();
 
+requestController["currentIterations"] = 0;
+
 requestController["currentSumId"] = currentSumId;
 
+// debug code below:
+//console.log(JSON.parse(fs.readFileSync("./divisionsAndTiers.json")));
+//return 1;
 // extract divisions and tiers and statuses of completion: (will have separate function to map non completed)
 requestController["divisionsAndTiers"] = filterNonFinishedDivsAndTiers(JSON.parse(fs.readFileSync("./divisionsAndTiers.json").toString()));
+//console.log(requestController["divisionsAndTiers"]);
+//return 1;
 
+requestController["writeStreams"] = {
+	currentSumId: fs.createWriteStream("./currentSumId"),
+	divisionsAndTiers: fs.createWriteStream("./divisionsAndTiers.json")
+};
 
-requestController["reqInterval"] = setInterval(handleRequest(this.currentSumId, this.divisionsAndTiers),1000);
+requestController["reqInterval"] = setInterval(function(){
+	handleRequest(this.currentSumId, this.divisionsAndTiers);
+},1500);
 
 requestController.on("request", function(error, data) {
-
+	//delete me later:
+	console.log("Request complete!");
 	//increment summonerid by appropriate number:
-	this.currentSumId += 8;
+	this.currentSumId += 33;
 
-	/* 
+	// increment counter
+	this.currentIterations++;
+
+
+	if (error) {
+		console.log("error!");
+		console.log(error);
+	}
+
+	else {
+		// call DB function, updateDB will emit response rather than use a callback here
+		console.log("calling db!");
+		updateDB(data);
+	}
+
+	/*
 
 	// format of data object
 
 	data {
-	sumId: 123456,
+	sumIds: [123456,353993....]
 	division: "Silver",
 	tier: "II"
 	}
@@ -54,32 +87,73 @@ requestController.on("request", function(error, data) {
 
 });
 
-requestController.on("processExiting", function(code) {
-	console.log(`Process exiting with code of: ${code}`);
-	console.log(`Now saving last summoner id used: ${this.currentSumId}`);
 
-	var ws = fs.createWriteStream("./config.js");
+// this event fired when database functionality is finished post request
+requestController.on("dbFinish", function(error, divAndTier) {
+	console.log("db fin called");
 
-	ws.write("module.exports = " + this.currentSumId);
-	ws.close();
+	if (error) {
+		// maybe switch to logging, since these error imply database failure (crucial point)
+		return console.log(error);
+	}
 
-	var ws2 = fs.createWriteStream("./divisionsAndTiers.json");
-	ws.write(JSON.stringify(divisionsAndTiers));
+	else if (divAndTier["completed"] === true) {
 
-	console.log("Progress has been saved successfully!");
+		// modify this.divsAndTiers to negate redundancy functionality ***
+
+		// lookup by index rather than iteration, since I'm fancy
+		var divAndTierMarker = {
+			tier: divAndTier["tier"],
+			division: divAndTier["division"]
+		};
+
+		var index = this.divisionAndTiers.indexOf(divAndTierMarker);
+
+		this.divisionAndTiers[index]["completed"] = true;
+
+	}
+
+	else {
+		console.log(divAndTier);
+		console.log("The above object has been updated or saved successfully!");
+	}
 
 })
 
 
 
+// fired when the process is disrupted
+requestController.on("processExiting", function(code) {
+	clearInterval(this.reqInterval);
+	console.log(`Process exiting with code of: ${code}`);
+	console.log(`Now saving last summoner id used: ${this.currentSumId}`);
+
+	this.writeStreams.currentSumId.write(String(this.currentSumId));
+	this.writeStreams.currentSumId.close();
+
+	this.writeStreams.divisionsAndTiers.write(JSON.stringify(this.divisionsAndTiers));
+	this.writeStreams.divisionsAndTiers.close();
+
+	console.log("Progress has been saved successfully!");
+	console.log("Process going down in one second!");
+	setTimeout(function(){
+		process.exit(1);
+	},1000);
+
+})
+
+
 // This will catch any Ctrl+C or other kill signal sent to the program
 // this is important to keep track of our progress
-process.on("exit", function(code) {
+process.on("SIGINT", function(code) {
 
 	requestController.emit("processExiting", code);
 
 })
 
+
+// This handleRequest call is to test a valid summoner id
+//handleRequest(19887289, requestController.divisionsAndTiers);
 
 
 
@@ -87,11 +161,12 @@ process.on("exit", function(code) {
 
 
 
-function handleRequest(sumId){
+function handleRequest(sumId, divsAndTiers){
 
 request("https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/" + sumId + "?" + apiKey, function(error, response, body) {
 
 	if (error || response.statusCode !== 200) {
+		console.log("Error with request!");
 		return requestController.emit("request", "Failed to process request.");
 	}
 
@@ -100,20 +175,48 @@ request("https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/" + sumId + "
 
 		for (var  i = 0; i < output[sumId].length; ++i) {
 
-			if (queueTypes.includes(output[sumId]["queue"])) {
+			console.log(queueTypes);
+			console.log(output[sumId][i]["queue"]);
+
+			if (queueTypes.includes(output[sumId][i]["queue"])) {
+
+				var quickInd = output[sumId][i];
 
 				var divAndTier = {
-					tier: output[sumId][i]["tier"],
-         			division: output[sumId][i]["entries"][0]["division"]
-         		};
+					tier: quickInd["tier"],
+         	division: quickInd["entries"][0]["division"],
+					sumIds: []
+         };
 
+
+				 var shortHandCheck = quickInd["tier"].substr(0,1) + quickInd["entries"][0]["division"];
+
+				 console.log(shortHandCheck);
+
+				 //console.log(divsAndTiers);
+
+         for (var j = 0; j < divsAndTiers.length; ++j) {
+
+				 	if (divsAndTiers[j]["shortHand"] == shortHandCheck) {
+
+						for (var j = 0; j < quickInd["entries"].length; ++j) {
+
+						divAndTier["sumIds"].push(quickInd["entries"][j]["playerOrTeamId"]);
+
+					 	}
+
+					}
+				}
+         return requestController.emit("request", null, divAndTier);
+        }
 
 
 			}
 
 		}
 
-	}
+		return requestController.emit("request", "No Data to process!");
+
 
 })
 
@@ -126,10 +229,85 @@ request("https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/" + sumId + "
 function filterNonFinishedDivsAndTiers(arr) {
 
 	return arr.filter(function(divAndTier) {
-		if (divAndTier["completed"] === false) 
-			return divAndTier;
+		if (divAndTier["completed"] === false)
+			return {tier: divAndTier["tier"], division: divAndTier["division"]};
 	})
 
 }
 
 
+
+//=========== Database Functionality ===============//
+
+function updateDB(data) {
+
+	console.log("db func called");
+
+	/*var addAndSaveResults = function(divAndTier, sumIds) {
+
+		sumIds.forEach(function(sumId) {
+			divAndTier["summonerIds"].push(sumId);
+		})
+
+		IdCollection.update({division: divAndTier["division"], tier: divAndTier["tier"]}, divAndTier, function(error, updated) {
+			if (error) return requestController.emit("dbFinish", error);
+			else requestController.emit("dbFinish", null, updated);
+		})
+
+	}*/
+
+	IdCollection.findByDivisionAndTier(data["division"], data["tier"], function(error, divAndTier) {
+		console.log("cb rdy!");
+
+		if (error) {
+			console.log("db err");
+			console.log(error);
+			return requestController.emit("dbFinish", error)
+		}
+
+		else if (!divAndTier) {
+			console.log("stuff being created for db")
+			var divAndTier = new IdCollection();
+			divAndTier["division"] = data["division"];
+			divAndTier["tier"] = data["tier"];
+			divAndTier["summonerIds"] = [];
+
+			data["sumIds"].forEach(function(sumId) {
+				if (divAndTier["summonerIds"].length >= 200) {
+					divAndTier["completed"] = true;
+					return requestController.emit("dbFinish", null, updated);
+				}
+				divAndTier["summonerIds"].push(sumId);
+			})
+
+			divAndTier.save();
+
+			return requestController.emit("dbFinish", null, divAndTier);
+
+		}
+
+		else {
+			console.log("more sumIds being added to db");
+			data["sumIds"].forEach(function(sumId) {
+				if (divAndTier["summonerIds"].length >= 200) {
+					divAndTier["completed"] = true;
+					return requestController.emit("dbFinish", null, updated);
+				}
+				divAndTier["summonerIds"].push(sumId);
+			})
+
+			IdCollection.update({division: divAndTier["division"], tier: divAndTier["tier"]}, divAndTier, function(error, updated) {
+				if (error) return requestController.emit("dbFinish", error);
+				else requestController.emit("dbFinish", null, updated);
+			})
+
+		}
+
+	})
+
+}
+
+
+setTimeout(function(){
+	console.log("fin");
+}, 10000000);
